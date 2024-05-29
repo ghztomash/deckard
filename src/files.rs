793aba63ad0540ef10467e3ea2fb4cba2d8d826b
Tomash Ghz <ghz.tomash@hotmail.com>
2024-05-30 01:27:29 +0200

    Add chrono and mime types

--- src/files.rs
+++ src/files.rs
@@ -1,9 +1,11 @@
+use chrono::prelude::*;
+use infer::Type;
 use std::{
-    fmt,
-    fmt::Display,
-    fs::{self, DirEntry, FileType, Metadata},
+    fmt::{self, Display},
+    fs::{self, DirEntry, FileType},
     os::unix::fs::MetadataExt,
     path::PathBuf,
+    time::SystemTime,
 };
 
 #[derive(Debug)]
@@ -46,55 +48,64 @@ impl Display for EntryType {
 
 #[derive(Debug)]
 pub struct FileEntry {
+    pub id: u64,
     pub path: PathBuf,
-    pub full_path: PathBuf,
     pub name: String,
-    pub stem: String,
-    pub extention: String,
+    pub prefix: String,
+    pub extention: Option<String>,
     pub file_type: EntryType,
-    pub metadata: Metadata,
+    pub created: DateTime<Local>,
+    pub modified: DateTime<Local>,
+    pub mime_type: Option<Type>,
     pub size: u64,
     pub depth: usize,
+    pub hash: Option<u64>,
+    pub processed: bool,
 }
 
 impl FileEntry {
     pub fn new(entry: DirEntry, depth: usize) -> Self {
         let metadata = entry.metadata().unwrap();
         Self {
-            path: entry.path(),
-            full_path: fs::canonicalize(&entry.path()).unwrap_or(entry.path()),
+            id: 0,
+            path: fs::canonicalize(&entry.path()).unwrap_or(entry.path()),
             name: entry.file_name().into_string().unwrap_or_default(),
-            stem: entry
+            prefix: entry
                 .path()
                 .file_stem()
+                .and_then(|os_str| os_str.to_str())
+                .map(|s| s.to_string())
                 .unwrap_or_default()
-                .to_str()
-                .unwrap_or_default()
+                .split(".")
+                .collect::<Vec<&str>>()[0]
                 .to_string(),
             extention: entry
                 .path()
                 .extension()
-                .unwrap_or_default()
-                .to_str()
-                .unwrap_or_default()
-                .to_string(),
+                .and_then(|os_str| os_str.to_str())
+                .map(|s| s.to_string()),
             file_type: EntryType::new(entry.file_type()),
-            metadata: metadata.clone(),
+            created: metadata.created().unwrap().into(),
+            modified: metadata.modified().unwrap().into(),
+            // mime_type: infer::get_from_path(entry.path()).unwrap(),
+            mime_type: None,
             size: metadata.size(),
             depth,
+            hash: None,
+            processed: false,
         }
     }
 }
 
 impl Display for FileEntry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        _ = write!(f, "{}", " ".to_string().repeat(self.depth));
+        _ = write!(f, "{}", "  ".to_string().repeat(self.depth));
         write!(
             f,
-            "{} {}B {}",
-            self.path.to_str().unwrap_or_default(),
+            "{}  {} {}B",
+            self.file_type,
+            self.name,
             self.size,
-            self.file_type
         )
     }
 }
