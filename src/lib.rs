fe6d5060e4b8fbce608279d42e3118d79c91c312
Tomash Ghz <ghz.tomash@hotmail.com>
2024-06-01 17:13:34 +0200

    Move search logic to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-mod files;
+mod file;
 
-use files::FileEntry;
+use file::FileEntry;
 use std::collections::{HashMap, HashSet};
 use std::{fs, path::Path, path::PathBuf};
 
-pub fn visit_dirs(dirs: HashSet<PathBuf>) -> HashMap<PathBuf, FileEntry> {
+pub fn index_dirs(dirs: HashSet<PathBuf>) -> HashMap<PathBuf, FileEntry> {
     let mut files: HashMap<PathBuf, FileEntry> = HashMap::new();
     let mut dirs: HashSet<PathBuf> = HashSet::from(dirs);
 
@@ -42,6 +42,55 @@ pub fn visit_dir(dir: &Path) -> (HashMap<PathBuf, FileEntry>, HashSet<PathBuf>) 
         }
     }
     (files, dirs)
+}
+
+pub fn process_files(files_index: &mut HashMap<PathBuf, FileEntry>) {
+    for (_, f) in files_index {
+        f.process();
+    }
+}
+
+pub fn find_matches(
+    files_index: &HashMap<PathBuf, FileEntry>,
+) -> HashMap<PathBuf, HashSet<PathBuf>> {
+    let mut file_matches: HashMap<PathBuf, HashSet<PathBuf>> = HashMap::new();
+    let mut vec_files = Vec::with_capacity(files_index.capacity());
+
+    // let mut vec_files: Vec<&mut FileEntry> = files_index.values_mut().into_iter().collect();
+
+    for (_, f) in files_index {
+        vec_files.push(f);
+    }
+
+    for i in 0..vec_files.len() {
+        for j in i + 1..vec_files.len() {
+            let f = &vec_files[i];
+            let ff = &vec_files[j];
+            let matching = f.compare(ff);
+            if matching {
+                match file_matches.get_mut(&f.path) {
+                    Some(ref mut v) => {
+                        v.insert(ff.path.clone());
+                    }
+                    None => {
+                        file_matches.insert(f.path.clone(), HashSet::from([ff.path.clone()]));
+                    }
+                };
+                match file_matches.get_mut(&ff.path) {
+                    Some(ref mut v) => {
+                        v.insert(f.path.clone());
+                    }
+                    None => {
+                        file_matches.insert(ff.path.clone(), HashSet::from([f.path.clone()]));
+                    }
+                };
+                //f.matches(ff.id);
+                //ff.matches(f.id);
+            }
+        }
+    }
+
+    file_matches
 }
 
 pub fn collect_paths<P: AsRef<Path>>(target_paths: Vec<P>) -> HashSet<PathBuf> {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::path::PathBuf;
-
 use clone_hunter::*;
 mod cli;
-mod files;
-
-use files::FileEntry;
+mod file;
 
 use colored::*;
-
 use std::time::Instant;
 
 #[tokio::main]
@@ -30,60 +23,25 @@ async fn main() {
     println!("Paths: {}", format!("{:?}", target_paths).yellow());
 
     let now = Instant::now();
-    let files_index = visit_dirs(target_paths);
-
+    let mut files_index = index_dirs(target_paths);
     let elapsed = now.elapsed();
-    // let mut vec_files = Vec::from_iter(files.values());
-    let mut vec_files = Vec::with_capacity(files_index.capacity());
     println!(
         "Indexed {} files in {}",
-        files_index.capacity().to_string().green(),
+        &files_index.len().to_string().green(),
         format!("{:.2?}", elapsed).blue()
     );
 
     let now = Instant::now();
-    for (_, mut f) in files_index.clone() {
-        f.process();
-        vec_files.push(f);
-    }
+    process_files(&mut files_index);
     let elapsed = now.elapsed();
     println!(
         "Processed {} files in {}",
-        files_index.capacity().to_string().green(),
+        &files_index.len().to_string().green(),
         format!("{:.2?}", elapsed).blue()
     );
 
-    let mut file_matches: HashMap<PathBuf, HashSet<PathBuf>> = HashMap::new();
-
     let now = Instant::now();
-    for i in 0..vec_files.len() {
-        for j in i + 1..vec_files.len() {
-            let f = &vec_files[i];
-            let ff = &vec_files[j];
-            let matching = f.compare(ff);
-            if matching {
-                match file_matches.get_mut(&f.path) {
-                    Some(ref mut v) => {
-                        v.insert(ff.path.clone());
-                    }
-                    None => {
-                        file_matches.insert(f.path.clone(), HashSet::from([ff.path.clone()]));
-                    }
-                };
-                match file_matches.get_mut(&ff.path) {
-                    Some(ref mut v) => {
-                        v.insert(f.path.clone());
-                    }
-                    None => {
-                        file_matches.insert(ff.path.clone(), HashSet::from([f.path.clone()]));
-                    }
-                };
-                //f.matches(ff.id);
-                //ff.matches(f.id);
-            }
-        }
-    }
-
+    let file_matches = find_matches(&files_index);
     let elapsed = now.elapsed();
     println!(
         "Found {} matches in {}",
