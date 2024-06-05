330fe9c763030fa9314be99eee5449eea1f60b2d
Tomash Ghz <ghz.tomash@hotmail.com>
2024-06-05 02:04:33 +0200

    Move binary to separate folder

--- src/bin/deckard.rs
+++ src/bin/deckard.rs
@@ -1,15 +1,12 @@
 use deckard::*;
-mod cli;
-mod file;
-mod index;
 
 use colored::*;
 use std::time::Instant;
 
-use index::FileIndex;
+use deckard::index::FileIndex;
 
-#[tokio::main]
-async fn main() {
+fn main() {
+    env_logger::init();
     let args = cli::cli().get_matches();
 
     let target_dirs = match args.get_many::<String>("params") {
@@ -67,5 +64,46 @@ async fn main() {
             name.yellow(),
             format!("{:?}", match_names).yellow()
         );
+    }
+}
+
+mod cli {
+    use clap::{command, value_parser, Arg, Command};
+
+    pub fn cli() -> Command {
+        command!()
+            .about("Find file duplicates")
+            .version(env!("CARGO_PKG_VERSION"))
+            .arg(
+                Arg::new("params")
+                    .value_name("PATH")
+                    .value_hint(clap::ValueHint::AnyPath)
+                    .value_parser(value_parser!(String))
+                    .help("List of paths to traverse")
+                    .num_args(1..),
+            )
+            .arg(
+                Arg::new("ignore_hidden")
+                    .short('i')
+                    .long("ignore_hidden")
+                    .action(clap::ArgAction::SetTrue)
+                    .help("Do not check hidden files"),
+            )
+            .arg(
+                Arg::new("depth")
+                    .short('d')
+                    .long("depth")
+                    .value_parser(value_parser!(usize))
+                    .help("Maximum depth to traverse")
+                    .num_args(1),
+            )
+            .arg(
+                Arg::new("threads")
+                    .short('t')
+                    .long("threads")
+                    .value_parser(value_parser!(usize))
+                    .help("Number of worker threads to use")
+                    .num_args(1),
+            )
     }
 }
