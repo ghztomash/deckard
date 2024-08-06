47648d27e68fabd1c146d7f99275769ff3f6c668
Tomash Ghz <ghz.tomash@hotmail.com>
2024-08-07 00:49:27 +0200

    Update benches

--- deckard/benches/process.rs
+++ deckard/benches/process.rs
@@ -1,13 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use deckard::collect_paths;
+use deckard::config::SearchConfig;
 use deckard::index::FileIndex;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("process test_files", |b| {
         b.iter(|| {
-            let mut index = black_box(FileIndex::new(black_box(collect_paths(vec![
-                "./test_files/",
-            ]))));
+            let mut index = black_box(FileIndex::new(
+                black_box(collect_paths(vec!["./test_files/"])),
+                SearchConfig::default(),
+            ));
             black_box(index.index_dirs());
             index.process_files();
         })
@@ -15,9 +17,10 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("process dont_exist", |b| {
         b.iter(|| {
-            let mut index = black_box(FileIndex::new(black_box(collect_paths(vec![
-                "./dont_exist/",
-            ]))));
+            let mut index = black_box(FileIndex::new(
+                black_box(collect_paths(vec!["./dont_exist/"])),
+                SearchConfig::default(),
+            ));
             black_box(index.index_dirs());
             index.process_files();
         })
